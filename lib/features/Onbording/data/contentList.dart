201ed3fe32b40bc2b1ef01b4a24886869c304e4d
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2024-12-03 13:41:39 +0200

    onbording

---
+++ lib/features/Onbording/data/contentList.dart
@@ -0,0 +1,25 @@
+import 'package:recipes/features/Onbording/data/modelContent.dart';
+
+List<UnbordingContent> contents = [
+  UnbordingContent(
+    title: 'Thousands of tested recipes',
+    image: 'assets/images/onboarding0.png',
+    discription: "There is no need to fear failure. Tested"
+    "recipes are guaranteed to work by our "
+    "professional chefs "
+  ),
+  UnbordingContent(
+    title: ' Explore Endless Recipes',
+    image: 'assets/images/onboarding1.jpg',
+    discription: "Find recipes tailored to your taste, dietary preferences, and skill "
+    "level. Your next favorite dish is just a click away! "
+   
+  ),
+  UnbordingContent(
+    title: 'Personalized for You',
+    image: 'assets/images/onboarding2.jpg',
+    discription: " Set your dietary preferences, favorite cuisines, and cooking goals"
+    "We’ll handle the res "
+  
+  ),
+];
---
+++ lib/features/Onbording/data/modelContent.dart
@@ -0,0 +1,7 @@
+class UnbordingContent {
+  String? image;
+  String? title;
+  String? discription;
+
+  UnbordingContent({this.image, this.title, this.discription});
+}
