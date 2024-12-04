0af5fc9b8f9a769a9aa49f87411596e19314982f
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2024-12-04 16:55:48 +0200

    refactor home

--- lib/features/home/ui/pages/home_view.dart
+++ lib/features/home/ui/pages/home_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:recipes/core/constants/colors.dart';
 import 'package:recipes/features/home/ui/widgets/GridViewItemsCounty.dart';
 import 'package:recipes/features/home/ui/widgets/appBarHomePage.dart';
+import 'package:recipes/features/home/ui/widgets/bodyHomePage.dart';
 import 'package:recipes/features/home/ui/widgets/listview_in_home.dart';
 
 
@@ -14,66 +15,7 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Appbarhomepage(),
-            const SizedBox(height: 20),
-             Text(
-              'What is your favorite country?',
-              style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-  height: MediaQuery.of(context).size.height*.14,
-  child: GridViewitemscountry()
-),
-
-             const SizedBox(height: 20),
-               Text(
-              'All Recipes ',
-              style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
-            ),
-          
-            const ListviewInHome(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-class Cotaineritemcountry extends StatelessWidget {
-  const Cotaineritemcountry({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-      decoration: BoxDecoration(
-        color: mainColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(5.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('🇸🇩', style: TextStyle(fontSize: 24)),
-            SizedBox(width: 8),
-            
-            Text(
-              'Palestineeeeee',
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
-      ),
+      body: Bodyhomepage(),
     );
   }
 }
---
+++ lib/features/home/ui/widgets/bodyHomePage.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:recipes/features/home/ui/widgets/appBarHomePage.dart';
+import 'package:recipes/features/home/ui/widgets/gridViewItemsCounty.dart';
+import 'package:recipes/features/home/ui/widgets/listview_in_home.dart';
+
+class Bodyhomepage extends StatelessWidget {
+  const Bodyhomepage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Appbarhomepage(),
+          const SizedBox(height: 20),
+           Text(
+            'What is your favorite country?',
+            style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+      height: MediaQuery.of(context).size.height*.14,
+      child: GridViewitemscountry()
+    ),
+    
+           const SizedBox(height: 20),
+             Text(
+            'All Recipes ',
+            style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+          ),
+        
+          const ListviewInHome(),
+        ],
+      ),
+    );
+  }
+}
+
+
