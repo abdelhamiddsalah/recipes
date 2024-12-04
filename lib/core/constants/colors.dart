b35e8841ae5bf9aa3994a781b56294472963a0eb
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2024-12-04 15:32:10 +0200

    b

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
  const Color mainColor=Color(0xffFB9400);
  const Color secondColor=Color(0xffF3F4F6);
-  const Color greyColor=Color(0xffF3F4F6);
+  const Color greyColor=Color(0xff64748B);
