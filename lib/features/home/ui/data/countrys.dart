0d29f6f3a2c606f13acb91771c30275387e758ae
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2024-12-06 15:39:01 +0200

    refactor

--- lib/features/home/ui/data/countrys.dart
+++ lib/features/home/ui/data/countrys.dart
@@ -1,21 +1,32 @@
 class Countrys {
   final String name;
   final String flag;
+  final String ap;
 
-  Countrys({required this.name, required this.flag});
+  Countrys( {required this.name, required this.flag,required this.ap,});
 }
 
-List<Countrys> data=[
-  Countrys(name:'Egypt' , flag: '🇪🇬'),
-  Countrys(name:'Syria' , flag: '🇸🇾'),
-  Countrys(name:'Lebanon' , flag: '🇬🇧'),
-  Countrys(name:'America' , flag: '🇺🇸'),
-  Countrys(name:'Italy' , flag: '🇮🇹'),
-  Countrys(name:'Palestine' , flag: '🇸🇩'),
-   Countrys(name:'Egypt' , flag: '🇪🇬'),
-  Countrys(name:'Syria' , flag: '🇸🇾'),
-  Countrys(name:'Lebanon' , flag: '🇬🇧'),
-  Countrys(name:'America' , flag: '🇺🇸'),
-  Countrys(name:'Italy' , flag: '🇮🇹'),
-  Countrys(name:'Palestine' , flag: '🇸🇩'),
+List<Countrys> data = [
+  Countrys(name: 'Italy', flag: '🇮🇹', ap: 'a'),
+  Countrys(name: 'Japan', flag: '🇯🇵', ap: 'b'),
+  Countrys(name: 'Mexico', flag: '🇲🇽', ap: 'c'),
+  Countrys(name: 'India', flag: '🇮🇳', ap: 'd'),
+  Countrys(name: 'France', flag: '🇫🇷', ap: 'e'),
+  Countrys(name: 'China', flag: '🇨🇳', ap: 'f'),
+  Countrys(name: 'Lebanon', flag: '🇱🇧', ap: 'g'),
+  Countrys(name: 'Turkey', flag: '🇹🇷', ap: 'h'),
+  Countrys(name: 'Thailand', flag: '🇹🇭', ap: 'i'),
+  Countrys(name: 'Greece', flag: '🇬🇷', ap: 'j'),
+  Countrys(name: 'Spain', flag: '🇪🇸', ap: 'k'),
+  Countrys(name: 'Egypt', flag: '🇪🇬', ap: 'l'),
+  Countrys(name: 'Morocco', flag: '🇲🇦', ap: 'm'),
+  Countrys(name: 'Netherlands', flag: '🇳🇱', ap: 'n'),
+  Countrys(name: 'Oman', flag: '🇴🇲', ap: 'o'),
+  Countrys(name: 'Pakistan', flag: '🇵🇰', ap: 'p'),
+  Countrys(name: 'Russia', flag: '🇷🇺', ap: 'r'),
+  Countrys(name: 'South Korea', flag: '🇰🇷', ap: 's'),
+  Countrys(name: 'Thailand', flag: '🇹🇭', ap: 't'),
+  Countrys(name: 'Vietnam', flag: '🇻🇳', ap: 'v'),
+  Countrys(name: 'Wales', flag: '🏴', ap: 'w'),
+  Countrys(name: 'Yemen', flag: '🇾🇪', ap: 'y'),
 ];
