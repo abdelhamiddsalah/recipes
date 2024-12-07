c89e413aa370fc1ec989983773ca6b044035a698
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2024-12-07 13:50:01 +0200

    shimmer home

--- lib/features/home/ui/widgets/listview_in_home.dart
+++ lib/features/home/ui/widgets/listview_in_home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:recipes/features/home/logic/cubit/home_cubit.dart';
 import 'package:recipes/features/home/ui/widgets/bottomItem.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ListviewInHome extends StatelessWidget {
   const ListviewInHome({super.key});
@@ -17,14 +18,62 @@ class ListviewInHome extends StatelessWidget {
             itemBuilder: (context, builder) {
               return Padding(
                 padding: const EdgeInsets.only(bottom: 20),
-                child: Bottomitem(recipes: state.resipes[builder],),
+                child: Bottomitem(
+                  recipes: state.resipes[builder],
+                ),
               );
             },
           );
         } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
+          return ListView.builder(
+             shrinkWrap: true,
+           physics: const NeverScrollableScrollPhysics(),
+                    padding: EdgeInsets.symmetric(vertical: 10),
+             itemCount: 4,
+             itemBuilder: (BuildContext context, int index) {
+               return   Padding(
+                 padding: const EdgeInsets.symmetric(vertical: 20.0),
+                 child: Material(
+                   color: Colors.white,
+                   borderRadius: BorderRadius.circular(20),
+                   elevation: 10.0,
+                   child: Shimmer.fromColors(
+                     baseColor:  Colors.grey,
+                     highlightColor:  Colors.white,
+                     child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                    ClipRRect(
+                 borderRadius: BorderRadius.circular(16),
+                 child: Container(
+                
+                   height: MediaQuery.of(context).size.height * .2,
+                   width:double.infinity,
+                  color: Colors.grey,
+                 ),),
+                SizedBox( height: 10,) ,
+                 Container(
+                   color: Colors.grey,
+                   height:10 ,
+                 width:  MediaQuery.of(context).size.width * .7,
+                 ),
+                  SizedBox( height: 10,) ,
+                   Container(
+                   color: Colors.grey,
+                   height:10 ,
+                 width:  MediaQuery.of(context).size.width * .4,
+                 ),
+                 SizedBox( height: 30,) ,
+                 ],
+               ),
+                   ),
+                 ),
+               );
+                   
+           
+          },
+           );
+      
         }
       },
     );
